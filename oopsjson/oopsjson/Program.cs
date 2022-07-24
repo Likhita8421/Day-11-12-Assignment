9822ceb46ca8c85ca11f4210462c651241f5147c
Likhita <leenupandagre24@gmail.com>
2022-07-25 02:43:22 +0530

    updated code

---
+++ oopsjson/oopsjson/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+oopsjson.StockClass json = new oopsjson.StockClass();
+json.group(@"C:\Users\hp\Downloads\Day-11-12-Assignment-main\Day-11-12-Assignment-main\oopsjson\oopsjson\AddingJsonFile.json");
+            
